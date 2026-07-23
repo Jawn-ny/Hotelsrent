110f31412fc0a5d072e817a9e430074fecf3308b
Jawn-ny <longxi0701@qq.com>
2026-07-23 21:48:18 +0800

    feat: add expense list API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+import json
+import os
+
 app = FastAPI()
 
 @app.get("/api/health")
 def health_check():
     return {"status":"ok"}
+
+def load_expenses():
+
+    file_path = "data/expenses.json"
+
+    if not os.path.exists(file_path):
+        return []
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        expenses = json.load(file)
+
+    return expenses
+
+@app.get("/api/expenses")
+def get_expenses():
+    return load_expenses()
